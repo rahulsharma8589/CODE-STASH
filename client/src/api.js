4248a997c9fa88e7a9be0757a76e30024633022a
rahulsharma8589 <162006754+rahulsharma8589@users.noreply.github.com>
2025-12-22 23:14:49 +0530

    local host problem solved

--- client/src/api.js
+++ client/src/api.js
@@ -1,9 +1,16 @@
+// import axios from "axios";
+
+
+// const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:8800/api" : "/api";
+
+// export const api = axios.create({
+//   baseURL: BASE_URL,
+// });
 import axios from "axios";
 
-// This checks: Are we running locally? Use localhost.
-// Are we online? Use the relative /api path.
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:8800/api" : "/api";
-
-export const api = axios.create({
-  baseURL: BASE_URL,
+// usage of /api lets the proxy handle the destination
+const api = axios.create({
+  baseURL: "/api",
 });
+
+export { api };
